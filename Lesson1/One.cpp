72656e0d7917b5c6f5f0ca43a689ef2bac91ea34
Michael Johnson <michael@johnson.computer>
2019-02-16 23:38:11 -0500

    Make Lesson1/One use common library

--- Lesson1/One.cpp
+++ Lesson1/One.cpp
@@ -16,42 +16,30 @@ enum RoyaltyOption { FIRST, SECOND, THIRD };
 
 // Forward declarations
 
-// Returns input from the user. The input is first validated against the type
-// specified in the template. Then, if validator is not empty, the input is
-// validated against the provided validator function. If the input passes both
-// of these validations, it is returned to the caller.
-template <typename T>
-T RequestInput(const std::string& prompt, std::function<bool(T)> validator);
 // Validator function for use in RequestInput. Validates that the number of
 // copies is greater than 0.
 bool ValidateCopies(int copies);
 // Validator function for use in RequestInput. Validates that the net price is
 // greater than 0.
 bool ValidateNetPrice(float net_price);
-// Prompts the user to ask if they would like to continue. Re-prompts on
-// invalid input. Once valid input is received, it returns the result.
-bool RequestContinue();
 // Calculates the royalties under a given royalty option using the copies and
 // net_price given.
 float CalculateRoyalties(RoyaltyOption option, int copies, float net_price);
 // Prints the royalties of each option and provides the best option based on
 // the number of copies and net_price given.
 void PrintRoyalties(int copies, float net_price);
-// HandleInvalidInput tells the user that their input was invalid and then
-// prepares cin for another attempt.
-void HandleInvalidInput();
 // RoyaltyOptionToString gives a proper name for a royalty option.
 std::string RoyaltyOptionToString(size_t option_index, bool lowercase);
 
 // Provides the primary run loop for the program.
 void Run() {
   do {
-    const int copies = RequestInput<int>(
+    const int copies = mjohnson::common::RequestInput<int>(
         "How many copies do you expect to sell? ", ValidateCopies);
-    const auto net_price = RequestInput<float>(
+    const auto net_price = mjohnson::common::RequestInput<float>(
         "What is the net price of the title? ", ValidateNetPrice);
     PrintRoyalties(copies, net_price);
-  } while (RequestContinue());
+  } while (mjohnson::common::RequestContinue());
 }
 
 float CalculateRoyalties(RoyaltyOption option, int copies, float net_price) {
@@ -158,88 +146,9 @@ void PrintRoyalties(int copies, float net_price) {
   std::cout << "|---------------------------" << std::endl << std::endl;
 }
 
-template <typename T>
-T RequestInput(const std::string& prompt, std::function<bool(T)> validator) {
-  bool valid = true;
-  T response;
-  do {
-    std::cout << prompt;
-    std::cin >> response;
-
-    if (std::cin.fail()) {  // cin.fail returns true when we attempt to
-                            // extract a type from the stream, but the data
-                            // that the user entered cannot be converted to
-                            // that type.
-      valid = false;
-      HandleInvalidInput();
-      continue;  // Fail fast and attempt another prompt
-    }
-
-    if (validator) {  // Validator has a bool operator that tells us whether
-                      // or not the function is empty
-      valid = validator(response);
-    } else {
-      valid = true;
-    }
-
-    if (!valid) {
-      HandleInvalidInput();
-    }
-  } while (!valid);
-
-  return response;
-}
-
-bool RequestContinue() {
-  while (true) {
-    std::cout << "Would you like to run the program again? ";
-
-    std::string response;
-    {  // Put this in its own scope to reduce scope pollution
-      char next_char;
-      do {
-        next_char = static_cast<char>(std::cin.peek());
-        if (next_char == '\r' || next_char == '\n') {
-          std::cin.get(next_char);  // Remove the newline from the buffer
-        }
-      } while (next_char == '\r' || next_char == '\n');  // Continue as long
-                                                         // as we encounter
-                                                         // newlines
-    }
-    std::getline(std::cin, response);
-    if (response.empty()) {
-      return false;  // The default option is to not continue
-    }
-
-    // Lowercase the response to make it easier to compare
-    std::transform(response.begin(), response.end(), response.begin(),
-                   ::tolower);
-    if (response == "y" || response == "yes") {
-      return true;
-    }
-    if (response == "n" || response == "no") {
-      return false;
-    }
-
-    std::cout << response
-              << " is an invalid response. Available responses are yes, y, "
-                 "no, or n."
-              << std::endl
-              << std::endl;
-  }
-}
-
 bool ValidateCopies(int copies) { return copies > 0; }
 
 bool ValidateNetPrice(float net_price) { return net_price > 0; }
-
-void HandleInvalidInput() {
-  std::cout << "The given input was invalid." << std::endl << std::endl;
-  std::cin.clear();               // Clear the error from cin
-  std::cin.ignore(0xFFFF, '\n');  // Ignore (hopefully) all of the user input
-                                  // currently in the buffer up to the next
-                                  // new line.
-}
 
 std::string RoyaltyOptionToString(size_t option_index, bool lowercase) {
   switch (option_index) {
