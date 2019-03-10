28f4ac9625371ab640d223335b68c2ad06d18008
Michael Johnson <michael@johnson.computer>
2019-03-10 18:10:37 -0400

    Pipe grep output to /dev/null in pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -9,7 +9,7 @@ unset SAVEIFS
 FAILED=0
 
 for CHANGED_FILE in "${CHANGED_FILES[@]}"; do
-	git show ":${CHANGED_FILE}" | clang-format -style=file -output-replacements-xml -assume-filename="${CHANGED_FILE}" - | grep '<replacement '
+	git show ":${CHANGED_FILE}" | clang-format -style=file -output-replacements-xml -assume-filename="${CHANGED_FILE}" - | grep '<replacement '>/dev/null
 	if [ $? -ne 1 ]; then
 		echo "${CHANGED_FILE}: Not clang-formatted"
 		FAILED=1
