89aa48cf3e1c3b5d2570cca2e1d8210e38544d0d
Michael Johnson <michael@johnson.computer>
2019-03-23 13:55:16 -0400

    Improve pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -6,14 +6,20 @@ CHANGED_FILES=($(git diff --cached --name-only --diff-filter=ACMRT | grep -E '\.
 IFS=$SAVEIFS
 unset SAVEIFS
 
-FAILED=0
+EXIT_STATUS=0
 
 for CHANGED_FILE in "${CHANGED_FILES[@]}"; do
-	git show ":${CHANGED_FILE}" | clang-format -style=file -output-replacements-xml -assume-filename="${CHANGED_FILE}" - | grep '<replacement '>/dev/null
+	git show ":${CHANGED_FILE}" | clang-format -style=file -output-replacements-xml -assume-filename="${CHANGED_FILE}" - | grep -v '#include' | grep '<replacement '>/dev/null
 	if [ $? -ne 1 ]; then
 		echo "${CHANGED_FILE}: Not clang-formatted"
-		FAILED=1
+		EXIT_STATUS=1
+	fi
+
+	git show ":${CHANGED_FILE}" | cpplint --filter=-readability/nolint --assume_filename="${CHANGED_FILE}" - >/dev/null 2>&1
+	if [ $? -ne 0 ]; then
+		echo "${CHANGED_FILE}: Failed cpplint"
+		EXIT_STATUS=1
 	fi
 done
 
-exit $FAILED
+exit $EXIT_STATUS
